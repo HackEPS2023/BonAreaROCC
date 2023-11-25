75608fb6cd40ad873dfb5a409eb051c60b6f973d
Xavier Llubes Cano <45287530+xavi1111@users.noreply.github.com>
2023-11-25 21:58:28 +0100

    [WIP] path finding

--- BonAreaROCC_ALG/path_finder.py
+++ BonAreaROCC_ALG/path_finder.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+import os
 from BonAreaROCC_ALG.customer import Customer
 from BonAreaROCC_ALG.ticket import Ticket
 from BonAreaROCC_ALG.product import Product
@@ -8,10 +9,13 @@ from BonAreaROCC_ALG.tile import Tile
 from BonAreaROCC_ALG.vertex import Vertex
 
 def main(arguments):
-    customers = get_customers()
-    tickets = get_tickets()
-    products = get_products()
-    tiles = get_tiles()
+    current_directory = os.getcwd()
+    data_directory = os.path.dirname(current_directory) + '/Data'
+
+    customers = get_customers(data_directory)
+    tickets = get_tickets(data_directory)
+    products = get_products(data_directory)
+    tiles = get_tiles(data_directory)
     for ticket_id in tickets.keys():
         ticket = tickets[ticket_id]
         customer = customers[ticket.customer_id]
@@ -19,18 +23,18 @@ def main(arguments):
         write_to_csv(path)
 
 
-def get_customers():
+def get_customers(data_directory):
     customers = {}
-    with open('C:/Users/xavie/Documents/hackeps23_bonarea_ROCC/data/hackathon_customers_properties.csv', newline='') as csv_file:
+    with open(data_directory + '/hackathon_customers_properties.csv', newline='') as csv_file:
         file_reader = csv.reader(csv_file, delimiter=';', quotechar='|')
         for row in file_reader:
             customers[row[0]] = Customer(row[0], row[1], row[2])
     return customers
 
 
-def get_tickets():
+def get_tickets(data_directory):
     tickets = {}
-    with open('C:/Users/xavie/Documents/hackeps23_bonarea_ROCC/data/hackathon_tickets.csv', newline='') as csv_file:
+    with open(data_directory + '/hackathon_tickets.csv', newline='') as csv_file:
         file_reader = csv.reader(csv_file, delimiter=';', quotechar='|')
         for row in file_reader:
             if row[4] in tickets:
@@ -42,17 +46,17 @@ def get_tickets():
     return tickets
 
 
-def get_products():
+def get_products(data_directory):
     products = {}
-    with open('C:/Users/xavie/Documents/hackeps23_bonarea_ROCC/data/hackathon_article_picking_time.csv', newline='') as csv_file:
+    with open(data_directory + '/hackathon_article_picking_time.csv', newline='') as csv_file:
         file_reader = csv.reader(csv_file, delimiter=';', quotechar='|')
         for row in file_reader:
             products[row[0]] = Product(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
     return products
 
-def get_tiles():
+def get_tiles(data_directory):
     tiles = {}
-    with open('C:/Users/xavie/Documents/hackeps23_bonarea_ROCC/data/planogram_table.csv', newline='') as csv_file:
+    with open(data_directory + '/planogram_table.csv', newline='') as csv_file:
         file_reader = csv.reader(csv_file, delimiter=';', quotechar='|')
         for row in file_reader:
             tiles[row[0] + ',' + row[1]] = Tile(row[0], row[1], row[2], row[3], row[4])
@@ -63,24 +67,24 @@ def find_path(ticket, customer, products, tiles):
     weighted_complete_graph = WeightedCompleteGraph(num_vertices)
     vertices = {}
     current_vertices = 1
-    for tile in tiles:
-        if tile.description == 'paso':
+    for position in tiles.keys():
+        if tiles[position].description == 'paso':
             continue
-        if tile.description == 'paso-entrada':
-            vertices[0] = Vertex(tile.x, tile.y, True)
-        elif tile.description == 'paso-salida':
-            vertices[current_vertices] = Vertex(tile.x, tile.y, False)
+        if tiles[position].description == 'paso-entrada':
+            vertices[0] = Vertex(tiles[position].x, tiles[position].y, True)
+        elif tiles[position].description == 'paso-salida':
+            vertices[current_vertices] = Vertex(tiles[position].x, tiles[position].y, False)
             current_vertices += 1
         else:
-            for product in products:
-                if tile.description == product.article_id:
-                    vertices[current_vertices] = Vertex(tile.picking_x, tile.picking_y, True)
+            for article_id in products:
+                if tiles[position].description == products[article_id].article_id:
+                    vertices[current_vertices] = Vertex(tiles[position].picking_x, tiles[position].picking_y, True)
                     current_vertices += 1
 
     for x in range(len(vertices)):
         for y in range(len(vertices)):
             if x != y:
-                weighted_complete_graph.add_edge(vertices[x], vertices[y], minimum_weight(vertices[x], vertices[y]))
+                weighted_complete_graph.add_edge(x, y, minimum_weight(vertices[x], vertices[y]))
 
     weighted_complete_graph.find_optimal_path(0, 3)
 
